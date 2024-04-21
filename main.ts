ff00084c7361a91e43b7024bfb84bc39134b67f5
mrgarth-teacher <110578347+mrgarth-teacher@users.noreply.github.com>
2024-04-21 11:52:11 +1000

    Added clear screen to make output clearer

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 basic.forever(function () {
     basic.showString("Mr G")
+    basic.clearScreen()
     basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
     basic.showString("Digitech!")
 })
